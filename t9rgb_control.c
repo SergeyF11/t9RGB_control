0b76e0c664bf54422ad59a9e19a80a886f9179b5
Sergey <sergey.fedotov@smilart.com>
2024-10-17 16:24:14 +0300

    Change version

--- t9rgb_control.c
+++ t9rgb_control.c
@@ -3,7 +3,7 @@ __author__ = "Sergey Fedotov"
 __copyright__ = "Copyright 2024 Sergey Fedotov <servifed2@gmail.com>"
 __license__ = "GPLv3-or-later"
 __email__ = "servifed2@gmail.com"
-__version__ = "0.2"
+__version__ = "0.2a"
 */
 
 //#define _GNU_SOURCE  // For BOTHER flag in termios2 
